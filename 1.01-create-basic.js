36a569063b69b6d8b9abc12959512f344702e026
KuoHungYi <kuohungyi619@gmail.com>
2021-09-01 17:20:29 +0800

    Add courses

---
+++ 1.01-create-basic.js
@@ -0,0 +1,10 @@
+// Here is an example of object
+
+let duck = {
+    name: "Aflac",
+    numLegs: 2
+  };
+
+// Things to remember
+// Use : instead of = in the object
+// Use , instead of ; in the object
